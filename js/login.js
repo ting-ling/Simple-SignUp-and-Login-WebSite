4b0c31532d815cf094f7a812e723f33f9ca4c67d
ting-ling <78134099+ting-ling@users.noreply.github.com>
2025-11-24 02:22:24 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,41 @@
+﻿const passwordField = document.getElementById("password");
+const showPasswordToggle = document.getElementById("showPasswordToggle");
+
+showPasswordToggle.addEventListener("change", function () {
+    if (this.checked) {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+});
+
+
+
+document.getElementById('LoginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const data = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+
+    fetch('http://localhost:5296/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        .then(async response => {
+            if (!response.ok) {
+                const text = await response.text();
+                throw new Error(text);
+            }
+            return response.text();
+        })
+        .then(result => {
+            alert('Success: ' + result);
+            window.location.href = 'index.html';
+        })
+        .catch(err => {
+            alert('Error: ' + err.message);
+        });
+});
---
+++ js/script.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("login").onclick = function () {
+    window.location.href = "login.html";
+}
+
+document.getElementById("signup").onclick = function () {
+    window.location.href = "signup.html";
+}
+
